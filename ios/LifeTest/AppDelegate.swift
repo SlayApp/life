7b20975ff5c9a634533fce3259f6ac3eec42ef31
naraB <baranyildirim.g@gmail.com>
2025-06-06 10:28:42 +0200

    fix: stop push notifs when in foreground

--- ios/LifeTest/AppDelegate.swift
+++ ios/LifeTest/AppDelegate.swift
@@ -4,11 +4,12 @@ import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 import RNBootSplash
+import UserNotifications
 
 @main
-class AppDelegate: ExpoAppDelegate {
+class AppDelegate: ExpoAppDelegate, UNUserNotificationCenterDelegate {
   var window: UIWindow?
-
+  
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
   
@@ -16,28 +17,39 @@ class AppDelegate: ExpoAppDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    UNUserNotificationCenter.current().delegate = self
+    
     let delegate = ReactNativeDelegate()
     let factory = ExpoReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
-
+    
     reactNativeDelegate = delegate
     reactNativeFactory = factory
     bindReactNativeFactory(factory)
-
+    
     window = UIWindow(frame: UIScreen.main.bounds)
-
+    
     factory.startReactNative(
       withModuleName: "LifeTest",
       in: window,
       launchOptions: launchOptions
     )
-
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
   
   override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-      return RCTLinkingManager.application(app, open: url, options: options)
-    }
+    return RCTLinkingManager.application(app, open: url, options: options)
+  }
+  
+  func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler:
+    @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+    completionHandler([])
+  }
 }
 
 class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
@@ -50,7 +62,7 @@ class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
     super.customize(rootView)
     RNBootSplash.initWithStoryboard("BootSplash", rootView: rootView)
   }
-
+  
   override func bundleURL() -> URL? {
 #if DEBUG
     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
